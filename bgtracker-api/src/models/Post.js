4f776636f856992fbe4858861833edc5d31c2014
Daniil Abdulov <118374108+DaniilAbdulov@users.noreply.github.com>
2023-09-06 10:38:22 +0500

    Update Post.js

--- bgtracker-api/src/models/Post.js
+++ bgtracker-api/src/models/Post.js
@@ -8,9 +8,10 @@ class Post extends BaseModel {
     static get jsonSchema() {
         return {
             type: "object",
-            required: ["user_id", "title", "body"],
+            required: ["user_id", "user_name", "title", "body"],
             properties: {
                 user_id: { type: "integer" },
+                user_name: { type: "string" },
                 title: { type: "string" },
                 body: { type: "string" },
             },
