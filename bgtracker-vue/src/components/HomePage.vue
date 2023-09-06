eb78b2efd04d31b83fb7781ba0a3d7030f025fc5
Daniil Abdulov <118374108+DaniilAbdulov@users.noreply.github.com>
2023-09-06 08:09:34 +0500

    Update HomePage.vue

--- bgtracker-vue/src/components/HomePage.vue
+++ bgtracker-vue/src/components/HomePage.vue
@@ -2,13 +2,6 @@
     <div>
         <div class="content" v-if="userIsAuthenticated">
             <h1>Hello</h1>
-            <h2>It's my auth page</h2>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Perspiciatis id at laboriosam, ipsa voluptatum in commodi eum
-                reiciendis hic rerum beatae vel nemo, quos quis! Possimus
-                provident vitae natus ipsum.
-            </p>
         </div>
         <div v-if="!userIsAuthenticated">Please login to view the content.</div>
         <div class="posts">
