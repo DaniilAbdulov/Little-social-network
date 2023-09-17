004240e11867ac6bacc3ac1613959f8c107c6f66
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-09-17 10:49:59 +0500

    UserModule optimization

--- client/src/store/UserModule.js
+++ client/src/store/UserModule.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const moment = require("moment");
+import { format, parseISO } from "date-fns";
+import { enUS } from "date-fns/locale";
+
 export const UserModule = {
     state: () => ({
         user: {
@@ -15,14 +17,30 @@ export const UserModule = {
     getters: {},
     mutations: {
         SET_INFO_ABOUT_FETCHED_USER(state, data) {
-            console.log(data.postsCount[0].count);
-            state.user.username = data.userInfo[0].username;
-            state.user.email = data.userInfo[0].email;
-            state.user.registration = data.userInfo[0].created_at;
-            state.user.postsCount = data.postsCount[0].count;
-            state.user.commentsCount = data.commentsCount[0].count;
-            state.user.likesCount = data.likesCount[0].count;
-            state.user.todosCount = data.todosCount[0].count;
+            const {
+                userInfo: [{ username, email }],
+                postsCount: [{ count: postsCount }],
+                commentsCount: [{ count: commentsCount }],
+                likesCount: [{ count: likesCount }],
+                todosCount: [{ count: todosCount }],
+            } = data;
+            Object.assign(state.user, {
+                username,
+                email,
+                postsCount,
+                commentsCount,
+                likesCount,
+                todosCount,
+            });
+        },
+        ADD_USER_REGISTRATED(state, data) {
+            const date = data.userInfo[0].created_at;
+            const formattedDate = format(
+                parseISO(date),
+                "dd MMMM yyyy'г. in' HH:mm",
+                { locale: enUS }
+            );
+            state.user.registration = formattedDate;
         },
     },
     actions: {
@@ -30,6 +48,7 @@ export const UserModule = {
             try {
                 const response = await axios.get("/api/user/info/");
                 commit("SET_INFO_ABOUT_FETCHED_USER", response.data);
+                commit("ADD_USER_REGISTRATED", response.data);
             } catch (error) {
                 console.log(error);
             }
