7fcf8be2338b601cd2221603393d30036b2e3d83
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-09-17 10:58:47 +0500

    LogSign optimization

--- client/src/store/LogSignModule.js
+++ client/src/store/LogSignModule.js
@@ -1,21 +1,20 @@
 import axios from "axios";
+
+const initializeAxiosHeaders = (token) => {
+    if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    } else {
+        delete axios.defaults.headers.common.Authorization;
+    }
+};
+
 export const LogSignModule = {
     state: () => ({
-        user: {
-            id: null,
-            email: "",
-            username: "",
-            role: "",
-        },
+        user: { id: null, email: "", username: "", role: "" },
         errorMessage: "",
         regErrorMessage: "",
-        regUser: {
-            regUserName: "",
-        },
+        regUser: { regUserName: "" },
     }),
-    //        username() {
-    //     return this.$store.state.lognsig.user.username;
-    // },для доступа в компонентах
     getters: {
         adminIsAuthenticated(state) {
             return !!state.user.id;
@@ -24,66 +23,57 @@ export const LogSignModule = {
             return !!state.user.id;
         },
     },
-    // ...mapGetters("lognsig", {
-    //     isAuthenticated: "isAuthenticated",
-    // }),для доступа в компонентах
     mutations: {
-        CURRENT_USER_FETCHED(state, user) {
-            state.user.id = user.id;
-            state.user.email = user.email;
-            state.user.username = user.username;
-            state.user.role = user.role;
+        setCurrentUser(state, user) {
+            Object.assign(state.user, user);
         },
-        SET_ERROR_MESSAGE(state, message) {
+        setErrorMessage(state, message) {
             state.errorMessage = message;
         },
-        SET_REGERROR_MESSAGE(state, regmessage) {
-            state.regErrorMessage = regmessage;
+        setRegErrorMessage(state, regMessage) {
+            state.regErrorMessage = regMessage;
         },
-        SET_REG_USER(state, newUser) {
+        setRegUser(state, newUser) {
             state.regUser.regUserName = newUser.username;
         },
     },
     actions: {
-        //        ...mapActions("lognsig", {
-        //     logInUser*название функции*: "logInUser*название функции*",
-        // }), для доступа в компонентах
-        async initialLoad(context) {
-            if (localStorage.bgtrackerjwt) {
-                axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}`;
+        async initialLoad({ commit }) {
+            const token = localStorage.getItem("bgtrackerjwt");
+            initializeAxiosHeaders(token);
+
+            if (token) {
                 try {
                     const res = await axios.get("/api/auth/currentUser");
-                    context.commit("CURRENT_USER_FETCHED", res.data.user);
+                    commit("setCurrentUser", res.data.user);
                 } catch (error) {
                     console.error("Error fetching currentUser:", error);
                 }
             }
         },
-        async logInUser({ commit, dispatch }, { username, password }) {
-            commit("SET_ERROR_MESSAGE", "");
+        async logInUser({ commit }, { username, password }) {
+            commit("setErrorMessage", "");
+
             try {
                 const response = await axios.post("/api/auth/login", {
                     username,
                     password,
                 });
                 const { user, token } = response.data;
-                commit("CURRENT_USER_FETCHED", user);
+
+                commit("setCurrentUser", user);
                 localStorage.setItem("bgtrackerjwt", token);
-                axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-                if (response.data.user.role != "admin") {
-                    this.getters.userIsAuthenticated;
-                } else {
-                    this.getters.adminIsAuthenticated;
-                }
+                initializeAxiosHeaders(token);
             } catch (error) {
-                commit("SET_ERROR_MESSAGE", error.response.data.message);
+                commit("setErrorMessage", error.response.data.message);
             }
         },
         async RegistrationUser(
-            { commit, state },
+            { commit },
             { username, password, email, repeatpassword }
         ) {
-            commit("SET_REGERROR_MESSAGE", "");
+            commit("setRegErrorMessage", "");
+
             try {
                 const response = await axios.post("/api/auth/register", {
                     username,
@@ -91,24 +81,24 @@ export const LogSignModule = {
                     email,
                     repeatpassword,
                 });
-                const { token } = response.data;
-                const newUser = response.data.user;
-                localStorage.setItem("bgtrackerjwt", token); // сохраняем токен нового пользователя в локальном хранилище
-                axios.defaults.headers.common.Authorization = `Bearer ${token}`; // задаем заголовок Authorization для всех последующих запросов
-                commit("SET_REG_USER", newUser); // обновляем информацию о новом пользователе в состоянии Vuex
+                const { user: newUser, token } = response.data;
+
+                localStorage.setItem("bgtrackerjwt", token);
+                initializeAxiosHeaders(token);
+                commit("setRegUser", newUser);
             } catch (error) {
-                commit("SET_REGERROR_MESSAGE", error.response.data.message);
+                commit("setRegErrorMessage", error.response.data.message);
             }
         },
-
         logOutUser({ commit }) {
-            commit("CURRENT_USER_FETCHED", {
+            commit("setCurrentUser", {
                 id: null,
                 email: "",
                 username: "",
+                role: "",
             });
             localStorage.removeItem("bgtrackerjwt");
-            axios.defaults.headers.common.Authorization = null;
+            initializeAxiosHeaders(null);
         },
     },
     namespaced: true,
