92c1704d876e05082d69d09b71f5ab9709e92de7
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-09-17 11:25:50 +0500

    todos optimization

--- server/src/routes/todos.js
+++ server/src/routes/todos.js
@@ -9,11 +9,10 @@ router.get(
     "/todosofuser/",
     authenticate,
     asyncHandler(async (req, res) => {
-        const user_id = req.userId;
         const todosOfUser = await Todos.query()
             .select("todos.id", "todos.body", "todos.completed")
             .join("users", "users.id", "todos.user_id")
-            .where("todos.user_id", user_id)
+            .where("todos.user_id", req.userId)
             .orderBy("todos.created_at", "desc");
 
         res.status(200).json({
@@ -27,22 +26,16 @@ router.post(
     "/newtodo",
     authenticate,
     asyncHandler(async (req, res) => {
-        const { body } = req.body;
         try {
-            const newTodo = await Todos.query()
-                .insert({
-                    user_id: req.userId,
-                    body,
-                    completed: false,
-                })
-                .skipUndefined();
+            const { body } = req.body;
+            const newTodo = await Todos.query().insert({
+                user_id: req.userId,
+                body,
+                completed: false,
+            });
+
             res.status(201).json({
-                todo: {
-                    id: newTodo.id,
-                    user_id: newTodo.user_id,
-                    body: newTodo.body,
-                    completed: newTodo.completed,
-                },
+                todo: { ...newTodo },
                 message: "Todo successfully created",
             });
         } catch (error) {
@@ -56,19 +49,15 @@ router.delete(
     "/deletetodo",
     authenticate,
     asyncHandler(async (req, res) => {
-        const { todo_id } = req.body;
         try {
+            const { todo_id } = req.body;
             const deletedTodo = await Todos.query().deleteById(todo_id);
 
-            if (deletedTodo) {
-                res.status(200).json({
-                    message: "Todo successfully deleted",
-                });
-            } else {
-                res.status(404).json({
-                    message: "Todo not found",
-                });
-            }
+            res.status(deletedTodo ? 200 : 404).json({
+                message: deletedTodo
+                    ? "Todo successfully deleted"
+                    : "Todo not found",
+            });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Failed to delete todo" });
@@ -80,24 +69,20 @@ router.patch(
     "/changecomplete",
     authenticate,
     asyncHandler(async (req, res) => {
-        const userId = req.userId;
-        const { todo_id } = req.body.data;
         try {
+            const { todo_id } = req.body.data;
             const todo = await Todos.query()
                 .findById(todo_id)
-                .where("user_id", userId);
+                .where("user_id", req.userId);
 
             if (todo) {
-                const updatedTodo = await Todos.query()
-                    .patch({
-                        completed: !todo.completed,
-                    })
-                    .where("user_id", userId)
-                    .where("id", todo_id)
-                    .returning("*"); // Return the updated todo
+                const [updatedTodo] = await Todos.query()
+                    .patch({ completed: !todo.completed })
+                    .where({ user_id: req.userId, id: todo_id })
+                    .returning("*");
 
                 res.status(200).json({
-                    todo: updatedTodo[0], // Return the first element of the updated todos array
+                    todo: updatedTodo,
                     message: "Todo completion status successfully updated",
                 });
             } else {
